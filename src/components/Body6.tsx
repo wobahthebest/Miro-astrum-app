7d46b746494cc890d0d28626d36a2e0b7f063a0f
wobahthebest <vebdevfrontend@gmail.com>
2025-03-29 17:51:22 +0500

    another milestone is compleated

--- src/components/Body6.tsx
+++ src/components/Body6.tsx
@@ -1,4 +1,39 @@
+import { MoveRight } from "lucide-react";
+import { useTranslations } from "next-intl";
+
 const Body6 = () => {
-  return <div>Body6</div>;
+  const t = useTranslations("Body4");
+
+  return (
+    <div className="space-y-10 mb-20">
+      <h1 className="text-6xl font-semibold">{t("title")}</h1>
+      <div className="grid grid-cols-7 gap-4">
+        <p className=" btn">{t("NavBar.brainstorming")}</p>
+        <p className=" btn">{t("NavBar.diagramming")}</p>
+        <p className=" btn">{t("NavBar.meetings-Workshops")}</p>
+        <p className=" btn">{t("NavBar.scrum-Events")}</p>
+        <p className=" btn">{t("NavBar.mapping")}</p>
+        <p className=" btn">{t("NavBar.research-Design")}</p>
+        <p className=" btn">{t("NavBar.strategic-Planning")}</p>
+      </div>
+      <div className="md:flex flex-grid  gap-20 md:justify-between justify-center space-y-10 items-center">
+        <div className="w-full h-full space-y-15">
+          <h1 className="text-4xl font-semibold">{t("sub-title")}</h1>
+          <p className="text-2xl">{t("paragraph")}</p>
+          <p className="flex text-lg text-primary btn-link items-center space-x-2 ">
+            <span>{t("link")}</span>
+            <MoveRight />
+          </p>
+        </div>
+        <div className="w-full">
+          <img
+            className="object-fill"
+            src={"./Brainstormwithoutshadow.png"}
+            alt="img"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Body6;
--- src/components/Body7.tsx
+++ src/components/Body7.tsx
@@ -1,4 +1,52 @@
+import { Check, MoveRight } from "lucide-react";
+import { useTranslations } from "next-intl";
+
 const Body7 = () => {
-  return <div>Body7</div>;
+  const t = useTranslations("Body5");
+
+  return (
+    <div className="space-y-10 mb-20">
+      <h1 className="text-6xl font-semibold">{t("title")}</h1>
+      <div className="grid grid-cols-7 gap-4">
+        <p className=" btn">{t("NavBar.UX-&-Design")}</p>
+        <p className=" btn">{t("NavBar.marketing")}</p>
+        <p className=" btn">{t("NavBar.product-management")}</p>
+        <p className=" btn">{t("NavBar.engineering")}</p>
+        <p className=" btn">{t("NavBar.consultants")}</p>
+        <p className=" btn">{t("NavBar.agile-coaches")}</p>
+        <p className=" btn">{t("NavBar.sales")}</p>
+      </div>
+      <div className="md:flex flex-grid  gap-20 md:justify-between justify-center space-y-10 items-center">
+        <div className="w-full h-full space-y-15">
+          <div className="space-y-5">
+            <p className="text-2xl text-zinc-600 flex items-center space-x-2">
+              <Check />
+              <span>{t("paragraph1")}</span>
+            </p>
+            <p className="text-2xl text-zinc-600 flex items-center space-x-2">
+              <Check />
+              <span>{t("paragraph2")}</span>
+            </p>
+            <p className="text-2xl text-zinc-600 flex mb-10 items-center space-x-2">
+              <Check />
+              <span>{t("paragraph3")}</span>
+            </p>
+
+            <p className="flex  text-lg text-primary btn-link items-center space-x-2 ">
+              <span>{t("link")}</span>
+              <MoveRight />
+            </p>
+          </div>
+          <div className="space-y-4">
+            <p className="text-2xl text-zinc-600">{t("paragraph4")}</p>
+            <img src={"./logosdesign.png"} alt="img" />
+          </div>
+        </div>
+        <div className="w-full">
+          <img src={"./uxdesignwithoutshadow.png"} alt="img" />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Body7;
